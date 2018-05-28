cc52f42b0f0a67a3745278551f87edce20a59fbf
yukariko <yukariko@github.com>
2018-05-28 12:00:24 +0000

    add login, registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,14 @@ from flask import Flask
 from flask import render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config
-from app.logins import LoginForm
-
+from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
